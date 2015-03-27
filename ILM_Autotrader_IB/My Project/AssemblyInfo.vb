ab9d2887ec969942e4379a21de6f4f499b2d1097
SaniG <ssg97@hotmail.com>
2015-03-27 00:21:40 -0700

    Populate repo with initial files

---
+++ ILM_Autotrader_IB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ILM_Autotrader_IB")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Windows User")> 
+<Assembly: AssemblyProduct("ILM_Autotrader_IB")> 
+<Assembly: AssemblyCopyright("Copyright © Windows User 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a33749a2-55ea-43f8-9fa6-76dd924421fa")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
